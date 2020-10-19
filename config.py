99b8936588a3c1e940543ed59409bc75e5c54967
Razvan-home <razfv12@protonmail.com>
2020-10-19 14:18:08 +0200

    Add pagination
    Signed-off-by: Razvan-home <razfv12@protonmail.com>

--- config.py
+++ config.py
@@ -8,10 +8,13 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # ...
+    # mail notification
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
+
+    # pagination
+    POSTS_PER_PAGE = 3
